06647c41c1ce841e60e8cb266f9c2dfcc47f9530
Rachit Jain <jainrachit2004@gmail.com>
2025-10-01 15:18:19 +0530

    Basic UI elements created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    );
   }
 }
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,52 @@
+import 'package:expense_tracker/widgets/expenses-list/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> registerExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 21.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add),),
+        ],
+        title: Text(
+          "Flutter ExpenseTracker",
+        ),
+      ),
+      body: Column(
+        children: [
+          Text("Chart"),
+          Expanded(
+            child: ExpensesList(
+                expenses: registerExpenses
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
